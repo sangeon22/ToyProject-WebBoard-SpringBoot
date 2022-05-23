92fbcc73312c55cadadaf0932346c79819d5c414
sangeon22 <tkddjsdl33@hanmail.net>
2022-05-23 17:37:04 +0900

    index bootstrap 적용 및 homeController test

---
+++ src/main/java/com/springboard/webboard/controller/homeController.java
@@ -0,0 +1,12 @@
+package com.springboard.webboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class homeController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
