b01e026c24c06d9147768fedf499a4733e8d721c
sangeon22 <tkddjsdl33@hanmail.net>
2022-05-23 17:17:52 +0900

    Initial commit

---
+++ src/main/java/com/springboard/webboard/WebboardApplication.java
@@ -0,0 +1,13 @@
+package com.springboard.webboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboard/webboard/WebboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboard.webboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
