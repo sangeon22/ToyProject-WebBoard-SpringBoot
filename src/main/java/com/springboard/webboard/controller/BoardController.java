dfafbfef11bb3e6224a00a67aad8bec217de5ea0
sangeon22 <tkddjsdl33@hanmail.net>
2022-05-25 17:37:11 +0900

    JPA를 이용한 게시판 조회 commit

--- src/main/java/com/springboard/webboard/controller/BoardController.java
+++ src/main/java/com/springboard/webboard/controller/BoardController.java
@@ -1,15 +1,27 @@
 package com.springboard.webboard.controller;
 
+import com.springboard.webboard.entity.Board;
+import com.springboard.webboard.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
+    @Autowired
+    private BoardRepository boardRepository;
+
     @GetMapping("/list")
-    public String list(){
+    public String list(Model model){
+        List<Board> boards = boardRepository.findAll();
+        model.addAttribute("boards", boards);
+
         return "board/list";
     }
 }
