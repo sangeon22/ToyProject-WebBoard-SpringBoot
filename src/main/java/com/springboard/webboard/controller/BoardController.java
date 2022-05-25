9409bd5ba5784e956adaa642bd87076673c579b9
sangeon22 <tkddjsdl33@hanmail.net>
2022-05-25 23:49:06 +0900

    Handling Form Submission - Controller 설계 commit

--- src/main/java/com/springboard/webboard/controller/BoardController.java
+++ src/main/java/com/springboard/webboard/controller/BoardController.java
@@ -5,8 +5,7 @@ import com.springboard.webboard.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +22,24 @@ public class BoardController {
         model.addAttribute("boards", boards);
 
         return "board/list";
+    }
+
+    @GetMapping("/form")
+    public String form(Model model, @RequestParam(required = false) Long id){
+        if (id == null){
+            model.addAttribute("board", new Board());
+        } else {
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board", board);
+        }
+
+        return "board/form";
+    }
+
+    @PostMapping("/form")
+    public String greetingSubmit(@ModelAttribute Board board){
+        boardRepository.save(board);
+
+        return "redirect:/board/list";
     }
 }
