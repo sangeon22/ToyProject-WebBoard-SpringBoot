477ff356c46da338e31e0065c96a83fff28d8b06
sangeon22 <tkddjsdl33@hanmail.net>
2022-06-01 23:13:49 +0900

    게시글 리스트 페이지 Paging 처리 commit

--- src/main/java/com/springboard/webboard/controller/BoardController.java
+++ src/main/java/com/springboard/webboard/controller/BoardController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -27,16 +28,20 @@ public class BoardController {
 
     @GetMapping("/list")
     public String list(Model model,
-                       Pageable pageable){
+                       @PageableDefault(size = 10) Pageable pageable) {
         Page<Board> boards = boardRepository.findAll(pageable);
+        int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
+        int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         model.addAttribute("boards", boards);
 
         return "board/list";
     }
 
     @GetMapping("/form")
-    public String form(Model model, @RequestParam(required = false) Long id){
-        if (id == null){
+    public String form(Model model, @RequestParam(required = false) Long id) {
+        if (id == null) {
             model.addAttribute("board", new Board());
         } else {
             Board board = boardRepository.findById(id).orElse(null);
@@ -48,9 +53,9 @@ public class BoardController {
 
     @PostMapping("/form")
     public String greetingSubmit(@Valid Board board,
-                                 BindingResult bindingResult){
+                                 BindingResult bindingResult) {
         boardValidator.validate(board, bindingResult);
-        if (bindingResult.hasErrors()){
+        if (bindingResult.hasErrors()) {
             return "board/form";
         }
 
