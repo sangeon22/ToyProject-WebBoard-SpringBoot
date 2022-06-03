d871cc6142f9604ffbc3db3ed59cfb69f66f2fd9
sangeon22 <tkddjsdl33@hanmail.net>
2022-06-03 17:02:00 +0900

    SpringSecurity 로그인 기능 추가 commit

--- src/main/java/com/springboard/webboard/Config/WebSecurityConfig.java
+++ src/main/java/com/springboard/webboard/Config/WebSecurityConfig.java
@@ -12,14 +12,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated()
-                .and()
+                    .antMatchers("/", "/home").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
                 .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
+                    .loginPage("/login")
+                    .permitAll()
+                    .and()
                 .logout()
-                .permitAll();
+                    .permitAll();
     }
 }
