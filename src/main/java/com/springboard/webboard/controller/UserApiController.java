7b53bfe7c70cf70922d3807c7fd367d268682df1
sangeon22 <tkddjsdl33@hanmail.net>
2022-06-06 20:30:11 +0900

    oneToMany JPA  commit

---
+++ src/main/java/com/springboard/webboard/controller/UserApiController.java
@@ -0,0 +1,61 @@
+package com.springboard.webboard.controller;
+
+import com.springboard.webboard.entity.Board;
+import com.springboard.webboard.entity.User;
+import com.springboard.webboard.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.util.StringUtils;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+class UserApiController {
+    @Autowired
+    private UserRepository repository;
+
+    @GetMapping("/users")
+    List<User> all() {
+      return repository.findAll();
+    }
+
+    @PostMapping("/users")
+    User newUser(@RequestBody User newUser) {
+        return repository.save(newUser);
+    }
+
+    // Single item
+
+    @GetMapping("/users/{id}")
+    User one(@PathVariable Long id) {
+
+        return repository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/users/{id}")
+    User replaceUser(@RequestBody User newUser, @PathVariable Long id) {
+
+        return repository.findById(id)
+                .map(user -> {
+//                    employee.setTitle(newUser.getTitle());
+//                    employee.setContent(newUser.getContent());
+//                    user.setBoards(newUser.getBoards());
+                    user.getBoards().clear();
+                    user.getBoards().addAll(newUser.getBoards());
+                    for(Board board : user.getBoards()){
+                        board.setUser(user);
+                    }
+                    return repository.save(user);
+                })
+                .orElseGet(() -> {
+                    newUser.setId(id);
+                    return repository.save(newUser);
+                });
+    }
+
+    @DeleteMapping("/users/{id}")
+    void deleteUser(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+}
