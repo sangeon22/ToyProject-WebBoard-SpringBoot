2d59e7c88b5871e2c1ba65882b7d377de981bf1a
sangeon22 <tkddjsdl33@hanmail.net>
2022-06-15 23:33:24 +0900

    회원가입 폼 변경

--- src/main/java/com/springboard/webboard/controller/AccountController.java
+++ src/main/java/com/springboard/webboard/controller/AccountController.java
@@ -4,6 +4,7 @@ import com.springboard.webboard.entity.User;
 import com.springboard.webboard.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +22,8 @@ public class AccountController {
     }
 
     @GetMapping("/register")
-    public String register(){
+    public String register(Model model){
+        model.addAttribute("user", new User());
         return "account/register";
     }
 
