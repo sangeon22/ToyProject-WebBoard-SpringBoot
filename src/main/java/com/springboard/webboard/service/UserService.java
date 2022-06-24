e18e8e88b315279d4906cb8be3d5944305948036
sangeon22 <tkddjsdl33@hanmail.net>
2022-06-24 15:51:46 +0900

    회원가입 중복 아이디 체크 추가

--- src/main/java/com/springboard/webboard/service/UserService.java
+++ src/main/java/com/springboard/webboard/service/UserService.java
@@ -17,12 +17,17 @@ public class UserService {
     private PasswordEncoder passwordEncoder;
 
     public User save(User user) {
-        String encodedPassword = passwordEncoder.encode(user.getPassword());
-        user.setPassword(encodedPassword);
+        User input_username = userRepository.findByUsername(user.getUsername());
+        if (input_username != null) {
+            throw new IllegalStateException("이미 사용 중인 회원 이름입니다.");
+        }
+
         user.setEnabled(true);
         Role role = new Role();
         role.setId(1L);
         user.getRoles().add(role);
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
 
         return userRepository.save(user);
     }
