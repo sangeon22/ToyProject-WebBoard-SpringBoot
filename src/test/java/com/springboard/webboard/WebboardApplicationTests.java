822c4d8350049e305ab6bb64ffcb0456d179f743
sangeon22 <tkddjsdl33@hanmail.net>
2022-12-16 22:19:45 +0900

    Refactor: AWS EC2 배포 의존성 추가 및 테스트코드 주석
    인텔리제이 전체 테스트는 문제가 없었지만, EC2 서버에 빌드시 문제가 발생함에 따라 잠시 주석처리

--- src/test/java/com/springboard/webboard/WebboardApplicationTests.java
+++ src/test/java/com/springboard/webboard/WebboardApplicationTests.java
@@ -1,13 +1,13 @@
-package com.springboard.webboard;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class WebboardApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.springboard.webboard;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class WebboardApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
